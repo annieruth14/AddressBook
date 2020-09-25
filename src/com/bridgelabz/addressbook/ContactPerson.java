bbe30fd76583113c65d7c6c1e42724415639de6a
annie <ruthannie3@gmail.com>
2020-09-25 22:20:52 +0530

    [Annie]Refactored: Editing existing contact person using their name

--- src/com/bridgelabz/addressbook/ContactPerson.java
+++ src/com/bridgelabz/addressbook/ContactPerson.java
@@ -2,10 +2,17 @@ package com.bridgelabz.addressbook;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class ContactPerson {
 
-	
+	private static ArrayList<AddressBook> new_list = new ArrayList<AddressBook>();
 	public static void main(String[] args) {
+		 
+		new_list = addPerson();
+		editContact();
+	}
+	
+	public static ArrayList addPerson() {
 		ArrayList<AddressBook> list = new ArrayList<AddressBook>(); 
 		Scanner sc = new Scanner(System.in);
 		String answer = "null"; 
@@ -34,9 +41,57 @@ public class ContactPerson {
 			System.out.println("Do you want to enter more person? Enter yes or no: ");
 			answer = sc.nextLine();
 		}
+		return list;
 		
 	}
+	public  static void editContact() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name to edit");
+		String name = sc.nextLine();
+		String search = "null";
+		
+		System.out.println("Enter the field you want to edit");
+		String field = sc.nextLine();
+		System.out.println("Enter the new value");
+		String new_value = sc.nextLine();
+		for(AddressBook b : new_list) {
+			if(name.equalsIgnoreCase(b.getFirst_name())) {
+				if(field.equalsIgnoreCase("address")) {
+					b.setAddress(new_value);
+				}
+				else if(field.equalsIgnoreCase("city")) {
+					b.setCity(new_value);
+				}
+				else if(field.equalsIgnoreCase("state")) {
+					b.setState(new_value);
+				}
+				else if(field.equalsIgnoreCase("zip")) {
+					int val = Integer.parseInt(new_value);
+					b.setZip(val);
+				}
+				else if(field.equalsIgnoreCase("phone")) {
+					long ph = Long.parseLong(new_value);
+					b.setPhone(ph);
+				}
+				else if(field.equalsIgnoreCase("email")) {
+					b.setEmail(new_value);
+				}
+				else
+					System.out.println("The name of the field is not correct");
+			}
+			
+		}
+		System.out.println("The details after editing are :");
+		for(AddressBook p : new_list) {
+			System.out.println(p.getFirst_name()+" "+ p.getLast_name()+" " + p.getAddress()+" "+ p.getCity()+" "+ p.getState()+" "+ p.getZip()+" "+ p.getPhone()+ " " +p.getEmail());
+
+		}
+		
+				
+	}
 }
+
+
 
 
 
